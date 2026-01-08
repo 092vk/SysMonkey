852fc7249f3853367eb9bae734394897a6e94758
092vk <vk092kumar@gmail.com>
2026-01-08 18:25:58 +0530

    (feat)- added gui partially

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #include "cpu.c"
 #include "mem.c"
+// #include "ui.c"
 
 
 int main(int argc , char *argv[]){
@@ -27,8 +28,12 @@ int main(int argc , char *argv[]){
         double memUsagePercent = memUsage();
 
         // formatting for proper display
-        printf("\nCPU_USAGE : %lf\n",cpu_usage);
-        printf("\nMEM_USAGE : %lf\n",memUsagePercent);
+        printf("\033[H\033[J");
+        printf("====================================\n");
+        printf("\n******* SysMonkey *******\n");
+        printf("\nCPU_USAGE : %lf %% \n",cpu_usage);
+        printf("\nMEM_USAGE : %lf %% \n",memUsagePercent);
+        fflush(stdout);
 
     }
     return 0;
